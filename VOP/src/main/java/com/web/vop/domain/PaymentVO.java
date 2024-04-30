e3827247b5a9c1d9f138adac83a32b9cade80f92
jian <yove242@naver.com>
2024-04-30 12:24:34 +0900

    vo 업로드

---
+++ VOP/src/main/java/com/web/vop/domain/PaymentVO.java
@@ -0,0 +1,20 @@
+package com.web.vop.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class PaymentVO {
+	private int paymentId;
+	private String memberId;
+	private String deliveryAddress;
+	private String receiverName;
+	private String receiverPhone;
+}
