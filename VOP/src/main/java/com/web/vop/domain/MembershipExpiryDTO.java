c7d3d349ceadf1a7a60b9453369a605ad627d59a
jian <yove242@naver.com>
2024-06-19 18:18:23 +0900

    멤버십 스케줄러 적용 완료

---
+++ VOP/src/main/java/com/web/vop/domain/MembershipExpiryDTO.java
@@ -0,0 +1,15 @@
+package com.web.vop.domain;
+
+import java.util.Date;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MembershipExpiryDTO {
+
+	private String memberId;
+	private Date expiryDate;
+	
+}//end MembershipExpiryDTO()
